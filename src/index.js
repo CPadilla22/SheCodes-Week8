7f70b94bc56a566fd2fc4253430b7955011f6823
CPadilla22 <129811774+CPadilla22@users.noreply.github.com>
2024-04-27 15:41:38 -0700

    Updated the forecast to include the day of the week

--- src/index.js
+++ src/index.js
@@ -47,6 +47,13 @@ function searchCity(city) {
   axios.get(apiUrl).then(refreshWeather);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -68,12 +75,13 @@ function getForecast(city) {
 function displayForecast(response) {
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date">Tue</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
     
     
     <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
@@ -86,6 +94,7 @@ function displayForecast(response) {
       )}°</div>
     </div>
   </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
